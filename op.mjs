eb217364d3a09b3dba757d1705481cbf0a7cfc49
karthikeyan65 <karthikeyan.k@meyicloud.com>
2024-12-06 18:00:32 +0530

    added operations

---
+++ op.mjs
@@ -0,0 +1,25 @@
+//ADD
+export function add(a,b)
+{
+    return a+b
+}
+
+//SUBTRACT
+export function sub(a,b)
+{
+    return a-b
+}
+
+//MULTIPLY
+export function mul(a,b)
+{
+    return a*b
+}
+
+//DIVIDE
+export function div(a,b)
+{
+    return a/b
+}
+
+console.log("Proceed the Operation...")
